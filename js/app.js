bafd5ca1a6895cb6db949457f43e26a434322cca
YOUR NAME HERE <ULLOA_SANTIAGO@YAHOO.COM>
2019-09-09 15:30:45 -0400

    worked on login/registration page

---
+++ js/app.js
@@ -0,0 +1,9 @@
+method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: "BLAHaaaaa@BLAH.COM",
+                password: "12345678a",
+                username: "BLAH aaaaaBLAH"
+            })
