96104293da2921f301974099bbbbd3aef779e487
YOUR NAME HERE <ULLOA_SANTIAGO@YAHOO.COM>
2019-09-09 16:26:30 -0400

    changed app

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
-method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: "BLAHaaaaa@BLAH.COM",
-                password: "12345678a",
-                username: "BLAH aaaaaBLAH"
-            })
+function getUserData() {
+    fetch('http://thesi.generalassemb.ly:8080/login')
+        .then((res) =>{
+            return res.json();
+        })
+        .then((res) => {
+            console.log(res);
+            return res;
+        })
+        .catch((err) =>{
+            console.log(error);
+        })
+}
+function makeUser() {
+    fetch('http://thesi.generalassemb.ly:8080/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: "BLAH@BLAH.COM",
+            password: "12345678",
+            username: "BLAH_BLAH"
+        })
+    })
+}
